9af061e9a5c07cf0ce5b454a8efe377643ceb9a7
Arjuna <redilberto@gmail.com>
2019-11-02 17:08:04 -0600

    Se agregarno modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,20 @@ import { AuthGuard } from './auth/auth.guard';
 const routes: Routes = [
 
   { path: 'bank',
-
   canActivate: [ AuthGuard ],
   loadChildren: './pages/bank/bank.module#BankModule'
-
+  },
+  { path: 'suppliers',
+  canActivate: [ AuthGuard ],
+  loadChildren: './pages/suppliers/suppliers.module#SuppliersModule'
+  },
+  { path: 'accounting-books',
+  canActivate: [ AuthGuard ],
+  loadChildren: './pages/accounting-books/accounting-books.module#AccountingBooksModule'
+  },
+  { path: 'customers',
+  canActivate: [ AuthGuard ],
+  loadChildren: './pages/customers/customers.module#CustomersModule'
   },
   { path: 'login', component: LoginComponent },
 
