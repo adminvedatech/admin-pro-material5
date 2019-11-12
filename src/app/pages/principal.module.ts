7ca71b84aef38673b393cf6e4f07fb10416cf8db
Arjuna <redilberto@gmail.com>
2019-11-11 20:38:22 -0600

    limpieza de modulos

--- src/app/pages/principal.module.ts
+++ src/app/pages/principal.module.ts
@@ -5,15 +5,7 @@ import { PAGES_ROUTES } from './principal-routing.module';
 import { PrincipalComponent } from './principal.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { CustomersModule } from './customers/customers.module';
-import { BankModule } from './bank/bank.module';
-import { AccountingBooksModule } from './accounting-books/accounting-books.module';
-import { ProductionsModule } from './productions/productions.module';
-import { ProductsModule } from './products/products.module';
-import { PurchasesModule } from './purchases/purchases.module';
-import { ReportsModule } from './reports/reports.module';
-import { SalesModule } from './sales/sales.module';
-import { SuppliersModule } from './suppliers/suppliers.module';
+
 
 @NgModule({
   declarations: [
@@ -21,29 +13,11 @@ import { SuppliersModule } from './suppliers/suppliers.module';
   ],
   imports: [
     CommonModule,
-    // AccountingBooksModule,
-    // BankModule,
-    // CustomersModule,
-    // ProductionsModule,
-    // ProductsModule,
-    // PurchasesModule,
-    // ReportsModule,
-    // SalesModule,
-    // SuppliersModule,
     PAGES_ROUTES,
     MatToolbarModule
   ],
   exports: [
-    // AccountingBooksModule,
-    // BankModule,
-    // CustomersModule,
-    // ProductionsModule,
-    // ProductsModule,
-    // PurchasesModule,
-    // ReportsModule,
-    // SalesModule,
-    // SuppliersModule,
-    MatToolbarModule
+     MatToolbarModule
   ]
 })
 export class PrincipalModule { }
